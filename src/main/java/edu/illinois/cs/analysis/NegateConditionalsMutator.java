1eca70bf6fe02a7ae1b227d5be37319c2766492e
Weikun Wu <weikunw@hotmail.com>
2023-03-31 21:25:15 -0500

    Add negate conditionals and primitive returns

---
+++ src/main/java/edu/illinois/cs/analysis/NegateConditionalsMutator.java
@@ -0,0 +1,52 @@
+package edu.illinois.cs.analysis;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.comments.BlockComment;
+import com.github.javaparser.ast.comments.JavadocComment;
+import com.github.javaparser.ast.comments.LineComment;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.NullLiteralExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+public class NegateConditionalsMutator extends VoidVisitorAdapter
+{
+
+	/**
+	 * This visit function will be automatically applied to all binary
+	 * expressions in the given Java file
+	 */
+	@Override
+	public void visit(BinaryExpr n, Object arg) {
+		super.visit(n, arg);
+        if (n.getOperator().equals(BinaryExpr.Operator.EQUALS)) {		
+			n.setOperator(BinaryExpr.Operator.NOT_EQUALS);
+			return;
+		}
+		if (n.getOperator().equals(BinaryExpr.Operator.NOT_EQUALS)) {		
+			n.setOperator(BinaryExpr.Operator.EQUALS);
+			return;
+		}
+		if (n.getOperator().equals(BinaryExpr.Operator.LESS_EQUALS)) {		
+			n.setOperator(BinaryExpr.Operator.GREATER);
+			return;
+		}
+		if (n.getOperator().equals(BinaryExpr.Operator.GREATER_EQUALS)) {		
+			n.setOperator(BinaryExpr.Operator.LESS);
+			return;
+		}
+        if (n.getOperator().equals(BinaryExpr.Operator.LESS)) {		
+			n.setOperator(BinaryExpr.Operator.GREATER_EQUALS);
+			return;
+		}
+		if (n.getOperator().equals(BinaryExpr.Operator.GREATER)) {		
+			n.setOperator(BinaryExpr.Operator.LESS_EQUALS);
+			return;
+		}
+	}
+}
---
+++ src/main/java/edu/illinois/cs/analysis/PrimitiveReturnsMutator.java
@@ -0,0 +1,37 @@
+package edu.illinois.cs.analysis;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.comments.BlockComment;
+import com.github.javaparser.ast.comments.JavadocComment;
+import com.github.javaparser.ast.comments.LineComment;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.IntegerLiteralExpr;
+import com.github.javaparser.ast.expr.NullLiteralExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+public class PrimitiveReturnsMutator extends VoidVisitorAdapter
+{
+
+	/**
+	 * This visit function will be automatically applied to all binary
+	 * expressions in the given Java file
+	 */
+	@Override
+	public void visit(MethodDeclaration n, Object arg) {
+		super.visit(n, arg);
+        if (n.getType().isPrimitiveType() && !n.getType().asString().equals("boolean")) {
+            if (n.getBody().isPresent()) {
+                BlockStmt block = n.getBody().get();
+                for (ReturnStmt rtn : block.findAll(ReturnStmt.class)) {
+                    rtn.setExpression(new IntegerLiteralExpr("0"));
+                }
+            }
+        }
+	}
+}
