4c0683793daca297f79a025ec19c95edcf6effdf
Ruhang <ruhang97@outlook.com>
2023-05-05 08:04:15 -0500

    create UOI mutators(not tested)

---
+++ src/main/java/edu/illinois/cs/analysis/UOI3.java
@@ -0,0 +1,48 @@
+package edu.illinois.cs.analysis;
+
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.UnaryExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+
+import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+
+public class UOI3 extends VoidVisitorAdapter
+{
+	@Override
+	public void visit(NameExpr n, Object arg) {
+		super.visit(n, arg);
+
+        // ResolvedValueDeclaration resolvedValueDeclaration = n.resolve();
+        // String type = resolvedValueDeclaration.getType().describe();
+        // if (type.equals("int") || type.equals("float")) {
+        // if (true) {
+        //     System.out.print("**mutated** Unary Op Insertion: " +
+        //         n.toString() + " -> ");
+        //     UnaryExpr unaryExpr = new UnaryExpr(n, UnaryExpr.Operator.MINUS);   // UOI3
+        //     // UnaryExpr unaryExpr = new UnaryExpr(n, UnaryExpr.Operator.PREFIX_INCREMENT);     // UOI2
+        //     // UnaryExpr unaryExpr = new UnaryExpr(n, UnaryExpr.Operator.POSTFIX_INCREMENT);    //UOI1
+        //     n.replace(unaryExpr);
+        //     // System.out.println(n.toString());
+        // }
+
+        try {
+            ResolvedType type = n.resolve().getType();
+            if (type.isPrimitive()) {
+                for (ResolvedType t : ResolvedPrimitiveType.getNumericPrimitiveTypes()) {
+                    if (type.asPrimitive() == t) {
+                        if (n.getParentNode().isPresent()) {
+                            n.getParentNode().get().replace(n, new UnaryExpr(n, UnaryExpr.Operator.MINUS));
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+        }
+	}
+}
+
