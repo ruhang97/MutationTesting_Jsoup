0c88a0c10097d71d7450157291e3c3d4761ba00a
Ruhang <ruhang97@outlook.com>
2023-05-05 18:46:40 -0500

    correct Math Mutator

--- src/main/java/edu/illinois/cs/analysis/MathMutator.java
+++ src/main/java/edu/illinois/cs/analysis/MathMutator.java
@@ -20,45 +20,46 @@ public class MathMutator extends VoidVisitorAdapter
 	@Override
 	public void visit(BinaryExpr n, Object arg) {
 		super.visit(n, arg);
-        if (!(n.getLeft() instanceof DoubleLiteralExpr && 
-                n.getRight() instanceof DoubleLiteralExpr) || 
-            !(n.getLeft() instanceof IntegerLiteralExpr && 
-                n.getRight() instanceof IntegerLiteralExpr)    ) {
-            return;
+        if (n.getLeft() instanceof DoubleLiteralExpr ||
+            n.getRight() instanceof DoubleLiteralExpr ||
+            n.getLeft() instanceof IntegerLiteralExpr ||
+            n.getLeft() instanceof IntegerLiteralExpr) {
+                
+            if (n.getOperator().equals(BinaryExpr.Operator.PLUS)) {		
+                // + -> -
+                n.setOperator(BinaryExpr.Operator.MINUS);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.MINUS)) {		
+                // - -> +
+                n.setOperator(BinaryExpr.Operator.PLUS);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.MULTIPLY)) {		
+                // * -> /
+                n.setOperator(BinaryExpr.Operator.DIVIDE);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.DIVIDE)) {		
+                // / -> *
+                n.setOperator(BinaryExpr.Operator.MULTIPLY);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.REMAINDER)) {
+                // % -> *
+                n.setOperator(BinaryExpr.Operator.MULTIPLY);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.BINARY_AND)) {
+                // & -> |
+                n.setOperator(BinaryExpr.Operator.BINARY_OR);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.BINARY_OR)) {
+                // | -> &
+                n.setOperator(BinaryExpr.Operator.BINARY_AND);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.XOR)) {
+                // ^ -> &
+                n.setOperator(BinaryExpr.Operator.BINARY_AND);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.LEFT_SHIFT)) {
+                // << -> >>
+                n.setOperator(BinaryExpr.Operator.SIGNED_RIGHT_SHIFT);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.SIGNED_RIGHT_SHIFT)) {
+                // >> -> <<
+                n.setOperator(BinaryExpr.Operator.LEFT_SHIFT);
+            } else if (n.getOperator().equals(BinaryExpr.Operator.UNSIGNED_RIGHT_SHIFT)) {
+                // >>> -> <<
+                n.setOperator(BinaryExpr.Operator.LEFT_SHIFT);
+            } 
+            
         }
-		if (n.getOperator().equals(BinaryExpr.Operator.PLUS)) {		
-            // + -> -
-			n.setOperator(BinaryExpr.Operator.MINUS);
-		} else if (n.getOperator().equals(BinaryExpr.Operator.MINUS)) {		
-            // - -> +
-            n.setOperator(BinaryExpr.Operator.PLUS);
-		} else if (n.getOperator().equals(BinaryExpr.Operator.MULTIPLY)) {		
-            // * -> /
-			n.setOperator(BinaryExpr.Operator.DIVIDE);
-		} else if (n.getOperator().equals(BinaryExpr.Operator.DIVIDE)) {		
-            // / -> *
-			n.setOperator(BinaryExpr.Operator.MULTIPLY);
-		} else if (n.getOperator().equals(BinaryExpr.Operator.REMAINDER)) {
-            // % -> *
-            n.setOperator(BinaryExpr.Operator.MULTIPLY);
-        } else if (n.getOperator().equals(BinaryExpr.Operator.BINARY_AND)) {
-            // & -> |
-            n.setOperator(BinaryExpr.Operator.BINARY_OR);
-        } else if (n.getOperator().equals(BinaryExpr.Operator.BINARY_OR)) {
-            // | -> &
-            n.setOperator(BinaryExpr.Operator.BINARY_AND);
-        } else if (n.getOperator().equals(BinaryExpr.Operator.XOR)) {
-            // ^ -> &
-            n.setOperator(BinaryExpr.Operator.BINARY_AND);
-        } else if (n.getOperator().equals(BinaryExpr.Operator.LEFT_SHIFT)) {
-            // << -> >>
-            n.setOperator(BinaryExpr.Operator.SIGNED_RIGHT_SHIFT);
-        } else if (n.getOperator().equals(BinaryExpr.Operator.SIGNED_RIGHT_SHIFT)) {
-            // >> -> <<
-            n.setOperator(BinaryExpr.Operator.LEFT_SHIFT);
-        } else if (n.getOperator().equals(BinaryExpr.Operator.UNSIGNED_RIGHT_SHIFT)) {
-            // >>> -> <<
-            n.setOperator(BinaryExpr.Operator.LEFT_SHIFT);
-        } 
 	}
 }
