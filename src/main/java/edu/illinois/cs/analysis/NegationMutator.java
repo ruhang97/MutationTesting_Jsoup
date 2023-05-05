fb0d3e97f7cdce7f693c81aa57a7ee52671fc81d
Weikun Wu <weikunw@hotmail.com>
2023-05-05 08:03:22 -0500

    Add field and array

--- src/main/java/edu/illinois/cs/analysis/NegationMutator.java
+++ src/main/java/edu/illinois/cs/analysis/NegationMutator.java
@@ -1,6 +1,8 @@
 package edu.illinois.cs.analysis;
 
+import com.github.javaparser.ast.expr.ArrayAccessExpr;
 import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
 import com.github.javaparser.ast.expr.UnaryExpr;
 import com.github.javaparser.ast.expr.NameExpr;
 
@@ -30,5 +32,47 @@ public class NegationMutator extends VoidVisitorAdapter
 
         }
 	}
+
+    @Override
+    public void visit(FieldAccessExpr n, Object arg) {
+        super.visit(n, arg);
+
+        try {
+            ResolvedType type = n.resolve().getType();
+            if (type.isPrimitive()) {
+                for (ResolvedType t : ResolvedPrimitiveType.getNumericPrimitiveTypes()) {
+                    if (type.asPrimitive() == t) {
+                        if (n.getParentNode().isPresent()) {
+                            n.getParentNode().get().replace(n, new UnaryExpr(n, UnaryExpr.Operator.MINUS));
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void visit(ArrayAccessExpr n, Object arg) {
+        super.visit(n, arg);
+
+        try {
+            ResolvedType type = n.calculateResolvedType();
+            if (type.isPrimitive()) {
+                for (ResolvedType t : ResolvedPrimitiveType.getNumericPrimitiveTypes()) {
+                    if (type.asPrimitive() == t) {
+                        if (n.getParentNode().isPresent()) {
+                            n.getParentNode().get().replace(n, new UnaryExpr(n, UnaryExpr.Operator.MINUS));
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
 }
 
